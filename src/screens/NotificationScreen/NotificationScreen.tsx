67fed135274ec05e42ed6b7eb9b8dc7bbfed50f2
shyfa.andiantono <shyfa.andiantono@62teknologi.com>
2024-06-30 13:02:59 +0700

    fix notification data list

--- src/screens/NotificationScreen/NotificationScreen.tsx
+++ src/screens/NotificationScreen/NotificationScreen.tsx
@@ -4,8 +4,9 @@ import Text from "components/Text";
 import api from "configs/api";
 import colors from "configs/colors";
 import icons from "configs/icons";
+import moment from "moment";
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, Image, SafeAreaView, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Dimensions, FlatList, Image, SafeAreaView, TouchableOpacity, View } from "react-native";
 import { useSelector } from "react-redux";
 import { StoreStateType } from "stores";
 import { useAuth } from "utils/hooks/UseAuth";
@@ -14,13 +15,16 @@ import { scaledHorizontal, scaledVertical } from "utils/ScaledService";
 import { convertDateHours, ObjectToURLSnake } from "utils/Utils";
 
 const NotificationScreen = () => {
+    const { height } = Dimensions.get('window');
     const { token } = useAuth();
+    const [firstLoad, setFirstLoad] = useState(true);
     const [dataNotification, setDataNotification] = useState([] as any);
     const [metaNotification, setMetaNotification] = useState({} as any);
+    const [page, setPage] = useState(1);
     const [isLoading, setLoading] = useState(false);
     const { isGetNotification } = useSelector((state: StoreStateType) => state.notification)
 
-    const _getDataNotifications = async (page: 1) => {
+    const _getDataNotifications = async (page: any) => {
         setLoading(true);
 
         const params = {
@@ -31,20 +35,77 @@ const NotificationScreen = () => {
         await api.getNotifications(token, queryParams).then((result) => {
             setDataNotification((prevData: any) => [...prevData, ...result?.data?.data])
             setMetaNotification(result?.data?.metadata)
+            setPage(result?.data?.metadata?.page)
         }).finally(() => {
             setLoading(false);
+            setFirstLoad(false);
         });
     }
 
-    useEffect(() => {
-        if (isGetNotification) {
-            _getDataNotifications(1);
+    const _renderTextNotification = (data: any) => {
+        let title = "cobaa ";
+        let message = "aoaaa";
 
+        const surgeryConst: any = {
+            1: {
+                title: "초진검진"
+            },
+            2: {
+                title: "1차 수술"
+            },
+            3: {
+                title: "2차 수술"
+            },
+            4: {
+                title: "본뜨기"
+            },
+            5: {
+                title: "중간과정"
+            },
+            6: {
+                title: "보철세팅"
+            },
+            7: {
+                title: "검진"
+            },
+        };
+
+        if (data?.message === "Surgery created.") {
+            title = `${surgeryConst?.[data?.userSurgeryDetail?.type]?.title}(${data?.userSurgeryDetail?.sort}회차) 예약`;
+            message = `${moment(data?.userSurgeryDetail?.reservatedAt).format("YYYY")}년 ${moment(data?.userSurgeryDetail?.reservatedAt).format("MM")}월 ${moment(data?.userSurgeryDetail?.reservatedAt).format("DD")}일 ${moment(data?.userSurgeryDetail?.reservatedAt).format("HH:mm")}에 예약되었습니다.`;
         }
-    }, []);
+
+        if (data?.message === "Surgery schedule changed.") {
+            title = `예약일정 변경`;
+            message = `${surgeryConst?.[data?.userSurgeryDetail?.type]?.title}(${data?.userSurgeryDetail?.sort}회차)이 ${moment(data?.userSurgeryDetail?.reservatedAt).format("YYYY")}년 ${moment(data?.userSurgeryDetail?.reservatedAt).format("MM")}월 ${moment(data?.userSurgeryDetail?.reservatedAt).format("DD")}일 ${moment(data?.userSurgeryDetail?.reservatedAt).format("HH:mm")}으로 일정이 변경되었습니다.`;
+        }
+
+        if (data?.message === "One day prior notif.") {
+            title = `${surgeryConst?.[data?.userSurgeryDetail?.type]?.title}(${data?.userSurgeryDetail?.sort}회차) 예약 - 1일 전`;
+            message = `${moment(data?.userSurgeryDetail?.reservatedAt).format("YYYY")}년 ${moment(data?.userSurgeryDetail?.reservatedAt).format("MM")}월 ${moment(data?.userSurgeryDetail?.reservatedAt).format("DD")}일 ${moment(data?.userSurgeryDetail?.reservatedAt).format("HH:mm")} 예약 1일 전입니다. 예약 시간에 방문해주시기를 바랍니다. 만약 예약 변경이 필요할 경우, 병원으로 연락해주세요.`;
+        }
+
+        if (data?.message === "Seven days prior notif.") {
+            title = `${surgeryConst?.[data?.userSurgeryDetail?.type]?.title}(${data?.userSurgeryDetail?.sort}회차) 예약 - 7일 전`;
+            message = `${moment(data?.userSurgeryDetail?.reservatedAt).format("YYYY")}년 ${moment(data?.userSurgeryDetail?.reservatedAt).format("MM")}월 ${moment(data?.userSurgeryDetail?.reservatedAt).format("DD")}일 ${moment(data?.userSurgeryDetail?.reservatedAt).format("HH:mm")} 예약 7일 전입니다. 예약 변경이 필요할 경우, 병원으로 연락해주세요.`;
+        }
+
+        if (data?.message === "Phone number changed.") {
+            title = `휴대전화 번호 변경 완료`;
+            message = `${data?.user?.name} ${moment(data?.createdAt).format("YYYY")}년 ${moment(data?.createdAt).format("MM")}월 ${moment(data?.createdAt).format("DD")}일 ${moment(data?.createdAt).format("HH:mm")}에 로그인 계정 정보인 '휴대전화 번호'가 변경되었습니다. 앞으로 변경된 휴대전화 번호로 로그인해주세요.`
+        }
+
+        return { title, message };
+    }
+
+    useEffect(() => {
+        if (isGetNotification && firstLoad) {
+            _getDataNotifications(page);
+        }
+    }, [isGetNotification]);
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#fff", height: height }}>
             <View style={{
                 height: 55,
                 paddingHorizontal: scaledHorizontal(20),
@@ -84,29 +145,45 @@ const NotificationScreen = () => {
                     <ActivityIndicator size={"large"} color={"#0f1e3d"} />
                 </View>
             ) : dataNotification?.length > 0 ? (
-                <FlatList
-                    data={dataNotification}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({ item }) => (
-                        <BaseCard
-                            key={item?.id}
-                            style={{
-                                backgroundColor: colors.white,
-                                paddingHorizontal: 15,
-                                paddingVertical: 17,
-                                marginHorizontal: 1,
-                                marginBottom: 18
-                            }}
-                        >
-                            <Text size={14} style={{ fontWeight: "bold", marginBottom: 8 }}>{item?.title}</Text>
-                            <Text size={13} color="#555" style={{ lineHeight: 22 }}>{item?.contents}</Text>
-                            <Space height={10} />
-                            <Text size={12} color="#999">{item?.sentAt ? convertDateHours(item?.sentAt) : ""}</Text>
-                        </BaseCard>
-                    )}
-                    onEndReached={() => _getDataNotifications(metaNotification?.page + 1)}
-                    onEndReachedThreshold={0.1}
-                />
+                <View
+                    style={{
+                        flex: 1,
+                    }}
+                >
+                    <FlatList
+                        data={dataNotification}
+                        style={{
+                            paddingHorizontal: 16,
+
+                        }}
+                        keyExtractor={(item) => item.id.toString()}
+                        initialNumToRender={10}
+                        renderItem={({ item }) => (
+                            <BaseCard
+                                key={item?.id}
+                                style={{
+                                    backgroundColor: colors.white,
+                                    paddingHorizontal: 15,
+                                    paddingVertical: 17,
+                                    marginHorizontal: 1,
+                                    marginBottom: 18
+                                }}
+                            >
+                                <Text size={14} style={{ fontWeight: "bold", marginBottom: 8 }}>{_renderTextNotification(item).title}</Text>
+                                <Text size={13} color="#555" style={{ lineHeight: 22 }}>{_renderTextNotification(item).message}</Text>
+                                <Space height={10} />
+                                <Text size={12} color="#999">{item?.createdAt ? convertDateHours(item?.createdAt) : ""}</Text>
+                            </BaseCard>
+                        )}
+                        onEndReached={() => {
+                            if (metaNotification?.lastPage > page) {
+                                _getDataNotifications(page + 1);
+                            }
+                        }}
+                        onEndReachedThreshold={0.5}
+                    />
+                </View>
+
             ) : (
                 <View
                     style={{
